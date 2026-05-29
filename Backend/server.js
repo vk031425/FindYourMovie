cd703ea6412e01adaa2e7112f9434ba745790d54
Vinay_ <140895233+vk031425@users.noreply.github.com>
2026-05-29 10:03:37 +0530

    updated port

--- Backend/server.js
+++ Backend/server.js
@@ -120,7 +120,7 @@ app.get("/", (req, res) => {
 
 // START SERVER
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
